4cf07bf7f8d10a68eb3df5ad53691a0e902b67a7
Furkan Sarı <furkansa@hotmail.com.tr>
2025-10-12 23:11:57 +0300

    feat: add /:postId route and ejs template

--- server/routers/indexRouter.js
+++ server/routers/indexRouter.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
-const { getAllPostsWithAuthors } = require("../controllers/queries");
+const {
+  getAllPostsWithAuthors,
+  getPostById,
+} = require("../controllers/queries");
 
 const indexRouter = Router();
 
@@ -11,7 +14,24 @@ indexRouter.get("/", async (_req, res, next) => {
       posts: posts,
     });
   } catch (err) {
-    return next(err);
+    next(err);
+  }
+});
+
+indexRouter.get("/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    const post = await getPostById(postId);
+    if (!post)
+      return res.status(404).render("pages/404", {
+        title: "Post Not Found",
+      });
+    res.render("pages/post", {
+      title: post.title,
+      post: post,
+    });
+  } catch (err) {
+    console.error("Error retrieving post", err);
   }
 });
 
