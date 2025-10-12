e9e83ef41f923c337bc1750ce0fde11a06d00dd4
Furkan Sarı <furkansa@hotmail.com.tr>
2025-10-12 14:24:22 +0300

    feat: add routes for /signup

--- server/app.js
+++ server/app.js
@@ -17,8 +17,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "layout");
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(
   session({
--- server/routers/signup.js
+++ server/routers/signup.js
@@ -1,11 +1,31 @@
 const { Router } = require("express");
+const pool = require("../db/pool.js");
+const { generateHash } = require("../controllers/passwordUtils.js");
+const { createNewUser } = require("../controllers/queries.js");
 
 const signupRouter = Router();
 
 signupRouter.get("/", (req, res) => {
-  res.send("pages/signupForm", {
+  res.render("pages/signup", {
     title: "Sign Up",
   });
+});
+
+signupRouter.post("/", async (req, res, next) => {
+  try {
+    const { hash, salt } = generateHash(req.body.password);
+    const newUser = {
+      username: req.body.username,
+      hash: hash,
+      salt: salt,
+      isAdmin: false,
+    };
+    await createNewUser(newUser);
+
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = signupRouter;
